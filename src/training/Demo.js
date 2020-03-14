fd5eec7bf49e8c741bdc0bb768e892ecb2740bda
unknown <vansang261198@gmail.com>
2020-03-14 08:36:29 +0700

    commit

--- src/training/Demo.js
+++ src/training/Demo.js
@@ -1,6 +1,9 @@
 // đầu tiên tạo ra cái Store, để quản lý tất cả các State của app
 import {createStore} from 'redux'
-// 1: khởi tạo state ban đầu
+import {status, sort} from './actions/index'
+
+
+// 1: khởi tạo state  ban đầu
 var initialState = {
     status: false,
     sort:{
@@ -10,40 +13,46 @@ var initialState = {
 }
 // 4: tạo ra reducer để nhận vào action, state
 var myReducer = (state = initialState, action) =>{
-    // if(action.type === 'TOGGLE_STATUS'){
-    //     state.status = !state.status
-    // }
-    if(action.type === 'SORT'){
+    if(action.type === 'TOGGLE_STATUS'){
+        state.status = !state.status
+        return state
+    }
+    else if(action.type === 'SORT'){
         state.sort = {
             by: action.sort.by,
             value:action.sort.value
         }
+        return state
     }
     return state
 }
 // 5: khởi tạo store, truyền reducer vào store
 const store  = createStore(myReducer)
+
 // console.log('DEFAULT: ' + store.getState())
 // thực hiện công việc thay đổi status (action)
 // var action = {
-//     type:'TOGGLE_STATUS'
+//     type:'TOGGLE_STATUS',
 // }
 
 // dùng hàm dispatch của store để truyền vào action đưa đến reducer
-// store.dispatch(action)
+store.dispatch(status())
 
-// console.log('TOGGLE_STATUS: ' + store.getState())
+console.log('TOGGLE_STATUS: ' + store.getState().status)
 
 // 2: khởi tạo action, thực hiện công việc sắp xếp: Sort name from Z-A
-var sortAction = {
-    type:'SORT',
-    sort:{
-        by:'name',
-        value:-1
-    }
-}
+// var sortAction = {
+//     type:'SORT',
+//     sort:{
+//         by:'name',
+//         value:-1
+//     }
+// }
 // 3: nhận dùng action sử dụng hàm dispatch
-store.dispatch(sortAction)
+store.dispatch(sort({
+    by:'name',
+    value:-1
+}))
 
 // 6: show ra store
-console.log('SORT: ' + store.getState())
+console.log('SORT: ' + store.getState().sort.value)
